c5d0ccbb7c096ef9b44d191f99af9d266a66383a
Vishwa Shanmugam <18vshanmugam.stem@gmail.com>
2017-11-10 17:21:51 +0000

    pushing static folder

---
+++ static/login.js
@@ -0,0 +1,27 @@
+function doLogin() {
+  
+  firebase.auth().signOut().then(function() {
+     var provider = new firebase.auth.GoogleAuthProvider();
+    console.log(provider);
+
+    var user = firebase.auth().currentUser;
+    if(!user) {
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+          var token = result.credential.accessToken;
+          var user = result.user;
+          console.log(user);
+          window.location.replace("main");
+        }).catch(function(error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          var email = error.email;
+          var credential = error.credential;
+          console.log("error " + errorMessage);
+        });
+    }
+  }, function(error) {
+    // An error happened.
+  });
+
+   
+}
